4b75759384223e2b6c5621ecf327bf138a947d1f
bibreen <bibreen@gmail.com>
2013-01-28 03:16:33 +0900

    * 수정: StandardPosAppender 품사 연결 규칙 추가 * 추가: make-jar.jardesc 추가

--- src/main/java/com/github/bibreen/mecab_ko_lucene_analyzer/StandardPosAppender.java
+++ src/main/java/com/github/bibreen/mecab_ko_lucene_analyzer/StandardPosAppender.java
@@ -22,24 +22,36 @@ public class StandardPosAppender extends PosAppender {
     appendableSet.add(new Appendable(PosId.E, PosId.E));
     // 어근(XR) + E [+ E]*
     appendableSet.add(new Appendable(PosId.XR, PosId.E));
-    // 용언(V*) + E [+ E]*
+    // 용언(V*)|동사 파생 접미사(XSV)|형용사 파생 접미사(XSA) + E [+ E]*
     appendableSet.add(new Appendable(PosId.VV, PosId.E));
     appendableSet.add(new Appendable(PosId.VA, PosId.E));
     appendableSet.add(new Appendable(PosId.VX, PosId.E));
     appendableSet.add(new Appendable(PosId.VCP, PosId.E));
     appendableSet.add(new Appendable(PosId.VCN, PosId.E));
+    appendableSet.add(new Appendable(PosId.XSV, PosId.E));
+    appendableSet.add(new Appendable(PosId.XSA, PosId.E));
     // 체언(N*) + 명사 파생 접미사(XSN)
     appendableSet.add(new Appendable(PosId.NN, PosId.XSN));
     appendableSet.add(new Appendable(PosId.NNB, PosId.XSN));
     appendableSet.add(new Appendable(PosId.NR, PosId.XSN));
     appendableSet.add(new Appendable(PosId.NP, PosId.XSN));
     appendableSet.add(new Appendable(PosId.COMPOUND, PosId.XSN));
-    // 체언(N*) + 조사 [+ 조사]*
-    appendableSet.add(new Appendable(PosId.NN, PosId.J));
-    appendableSet.add(new Appendable(PosId.NNB, PosId.J));
-    appendableSet.add(new Appendable(PosId.NR, PosId.J));
-    appendableSet.add(new Appendable(PosId.NP, PosId.J));
-    appendableSet.add(new Appendable(PosId.COMPOUND, PosId.J));
+    // 체언(N*)|일반부사(MAG)|어근(XR) + 동사 파생 접미사(XSV)
+    appendableSet.add(new Appendable(PosId.NN, PosId.XSV));
+    appendableSet.add(new Appendable(PosId.NNB, PosId.XSV));
+    appendableSet.add(new Appendable(PosId.NR, PosId.XSV));
+    appendableSet.add(new Appendable(PosId.NP, PosId.XSV));
+    appendableSet.add(new Appendable(PosId.COMPOUND, PosId.XSV));
+    appendableSet.add(new Appendable(PosId.MAG, PosId.XSV));
+    appendableSet.add(new Appendable(PosId.XR, PosId.XSV));
+    // 체언(N*)|일반부사(MAG)|어근(XR) + 형용사 파생 접미사(XSA)
+    appendableSet.add(new Appendable(PosId.NN, PosId.XSA));
+    appendableSet.add(new Appendable(PosId.NNB, PosId.XSA));
+    appendableSet.add(new Appendable(PosId.NR, PosId.XSA));
+    appendableSet.add(new Appendable(PosId.NP, PosId.XSA));
+    appendableSet.add(new Appendable(PosId.COMPOUND, PosId.XSA));
+    appendableSet.add(new Appendable(PosId.MAG, PosId.XSA));
+    appendableSet.add(new Appendable(PosId.XR, PosId.XSA));
     // 체언(N*) + 긍정/부정 지정사(VCP, VCN)
     appendableSet.add(new Appendable(PosId.NN, PosId.VCP));
     appendableSet.add(new Appendable(PosId.NNB, PosId.VCP));
@@ -54,8 +66,16 @@ public class StandardPosAppender extends PosAppender {
     // 명사 파생 접미사(XSN) + 긍정/부정 지정사(VCP, VCN)
     appendableSet.add(new Appendable(PosId.XSN, PosId.VCP));
     appendableSet.add(new Appendable(PosId.XSN, PosId.VCN));
+    // 체언(N*) + 조사 [+ 조사]*
+    appendableSet.add(new Appendable(PosId.NN, PosId.J));
+    appendableSet.add(new Appendable(PosId.NNB, PosId.J));
+    appendableSet.add(new Appendable(PosId.NR, PosId.J));
+    appendableSet.add(new Appendable(PosId.NP, PosId.J));
+    appendableSet.add(new Appendable(PosId.COMPOUND, PosId.J));
     // 명사 파생 접미사(XSN) + 조사(J)
     appendableSet.add(new Appendable(PosId.XSN, PosId.J));
+    // 어미(E) + 조사(J) - 어미가 명사형 전성 어미인 경우
+    appendableSet.add(new Appendable(PosId.E, PosId.J));
     // 조사(J) + 조사(J)
     appendableSet.add(new Appendable(PosId.J, PosId.J));
   }
