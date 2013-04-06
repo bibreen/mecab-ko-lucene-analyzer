e1988bff0f1fdf3648c206d68f3649db2f8a1b54
amitabul <mousegood@gmail.com>
2013-04-06 19:38:36 +0900

    * 수정: TokenGeneratorMockTest 특수문자 테스트 추가.

--- src/test/java/com/github/bibreen/mecab_ko_lucene_analyzer/TokenGeneratorMockTest.java
+++ src/test/java/com/github/bibreen/mecab_ko_lucene_analyzer/TokenGeneratorMockTest.java
@@ -192,6 +192,7 @@ public class TokenGeneratorMockTest {
     assertEquals(null, tokens);
   }
   
+  @Test
   public void testSymbolOnlySentence() {
     // ".,;:~"
     Node symbolsNode = mockNodeFactory(
